a7d8f0911f19778c41a6ffbe36b86c0294f78d1a
Yusuke Yamamoto <yusuke@mac.com>
2022-02-01 13:44:39 +0900

    initial commit

---
+++ src/main/java/YenTest.java
@@ -0,0 +1,10 @@
+public class YenTest {
+    public static void main(String[] args) {
+        char yen ='¥';
+        char backslash = '\\';
+        System.out.println((int)yen);
+        System.out.println((int)backslash);
+        System.out.println(yen);
+        System.out.println(backslash);
+    }
+}
